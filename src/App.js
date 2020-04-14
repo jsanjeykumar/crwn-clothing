bb58aea0aabb15f8b3e8d2be4477e668c65890e6
san_j <jsanjeykumr@gmail.com>
2020-04-15 00:00:10 +0530

    shop,preview,header component added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./pages/homepage/homepage.component";
 import { BrowserRouter,Switch, Route } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./component/header/header.component"
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+      <Header/>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
       </BrowserRouter>
     </div>
